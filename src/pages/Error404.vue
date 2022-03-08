abd316eda8d83a25dd576a1dbd77b7d9f28d9c9b
oceansam <oceansam101@gmail.com>
2022-03-07 23:16:55 -0500

    [CHORE]: Deploy

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div
+    class="fullscreen bg-white text-blue text-center q-pa-md flex flex-center"
+  >
+    <img src="../assets/floating-pepe.gif" />
     <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+      <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
+      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
 
       <q-btn
         class="q-mt-xl"
-        color="white"
-        text-color="blue"
+        color="blue"
+        text-color="white"
         unelevated
         to="/"
         label="Go Home"
@@ -23,9 +22,9 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Error404'
-})
+  name: "Error404",
+});
 </script>
